32bfa0bed472eccf3d6a0be7c29abae84443b340
Souvik Das <777souvik@gmail.com>
2021-03-17 19:45:07 +0530

    testcases increased

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -25,4 +25,63 @@ public class CalculatorTest {
         double ans=8.0;
         Assert.assertEquals(ans, calculator.root(64),0);
     }
+
+
+    @Test
+    public void testPower1()
+    {
+        double ans=64.0;
+        Assert.assertEquals(ans, calculator.power(4,3),0);
+    }
+    @Test
+    public void testFac1()
+    {
+        double ans=720;
+        Assert.assertEquals(ans, calculator.factorial(6),0);
+    }
+    @Test
+    public void testRoot1()
+    {
+        double ans=19.0;
+        Assert.assertEquals(ans, calculator.root(361),0);
+    }
+
+    @Test
+    public void testPower2()
+    {
+        double ans=256.0;
+        Assert.assertEquals(ans, calculator.power(4,4),0);
+    }
+    @Test
+    public void testFac2()
+    {
+        double ans=24;
+        Assert.assertEquals(ans, calculator.factorial(4),0);
+    }
+    @Test
+    public void testRoot2()
+    {
+        double ans=15.0;
+        Assert.assertEquals(ans, calculator.root(225),0);
+    }
+
+    @Test
+    public void testPower3()
+    {
+        double ans=27.0;
+        Assert.assertEquals(ans, calculator.power(3,3),0);
+    }
+    @Test
+    public void testFac3()
+    {
+        double ans=2;
+        Assert.assertEquals(ans, calculator.factorial(2),0);
+    }
+    @Test
+    public void testRoot3()
+    {
+        double ans=14.0;
+        Assert.assertEquals(ans, calculator.root(196),0);
+    }
+
 }
